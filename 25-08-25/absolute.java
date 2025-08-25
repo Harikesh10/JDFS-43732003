25831e5cb6be6af6e2262705f2b758968894285e
Harikesh <154328020+Harikesh10@users.noreply.github.com>
2025-08-25 21:04:48 +0530

    Create absolute.java

---
+++ 25-08-25/absolute.java
@@ -0,0 +1,11 @@
+package practise_java;
+import java.util.Scanner;
+
+public class demo3 {
+    public static void main(String args[]) {
+    	Scanner scan = new Scanner(System.in);
+    	int value = scan.nextInt();
+    	int answer= Math.abs(value);
+    	System.out.println(answer);   
+    }
+}
