75bdc8da11330e4fbab2a46083430ac161ef4366
Harikesh <154328020+Harikesh10@users.noreply.github.com>
2025-11-27 09:47:19 +0530

    Update Arra.java

--- Java25-11-25/Arra.java
+++ Java25-11-25/Arra.java
@@ -9,61 +9,62 @@ public class Arra {
 		
 	}
 	}
-//		int a[]={1,2,3,4,5};
-//		System.out.println(a[1]);
+		int a[]={1,2,3,4,5};
+		System.out.println(a[1]);
+		
+//		1)
+		String a1[] = {"a","b","c"};
+		System.out.println(a1[1]);
+		int a2[]={1,2,3,4,5};
+		System.out.println(a2);
+		
+		String newArray = Arrays.toString(a1);
+		System.out.println(newArray);
+		
+//		3)
+		String[] stringArray = { "abc", "bcd", "cba", "dcb", "efg" };
+
+		boolean b = Arrays.asList(stringArray).contains("abc");
+		boolean c = Arrays.asList(stringArray).contains("ddd");
+		System.out.println(b);
+		System.out.println(c);
+		
+//		4)
+		int[] d = {1,2,3};
+		int[] e = {9,9,9};
+		
+		int total[] = ArrayUtils.addAll(d,e);
+		System.out.println(total);
+		
+		String s = Arrays.toString(total);
+		System.out.println(s);
+		
+//		5)
+		
+		String aa = StringUtils.join(new String[] { "a", "b", "c" }, ", ");
+		System.out.println(aa);
+		
+//		6)
+		String[] s1 = { "a", "b", "c", "d", "e", "a" };
+
+
+		Set<String> set123 = new HashSet<String>(Arrays.asList(s1));
+		System.out.println(set123);
+		
+		
+//		7)
+		
+		int[] aaa = { 1, 2, 3, 4, 5 };
+		ArrayUtils.reverse(aaa);
+		System.out.println(Arrays.toString(aaa));
 //		
-////		1)
-//		String a1[] = {"a","b","c"};
-//		System.out.println(a1[1]);
-//		int a2[]={1,2,3,4,5};
-//		System.out.println(a2);
-//		
-//		String newArray = Arrays.toString(a1);
-//		System.out.println(newArray);
-//		
-////		3)
-//		String[] stringArray = { "abc", "bcd", "cba", "dcb", "efg" };
-//
-//		boolean b = Arrays.asList(stringArray).contains("abc");
-//		boolean c = Arrays.asList(stringArray).contains("ddd");
-//		System.out.println(b);
-//		System.out.println(c);
-//		
-////		4)
-//		int[] d = {1,2,3};
-//		int[] e = {9,9,9};
-//		
-//		int total[] = ArrayUtils.addAll(d,e);
-//		System.out.println(total);
-//		
-//		String s = Arrays.toString(total);
-//		System.out.println(s);
-//		
-////		5)
-//		
-//		String aa = StringUtils.join(new String[] { "a", "b", "c" }, ", ");
-//		System.out.println(aa);
-//		
-////		6)
-//		String[] s1 = { "a", "b", "c", "d", "e", "a" };
-//
-//
-//		Set<String> set123 = new HashSet<String>(Arrays.asList(s1));
-//		System.out.println(set123);
-//		
-//		
-////		7)
-//		
-//		int[] aaa = { 1, 2, 3, 4, 5 };
-//		ArrayUtils.reverse(aaa);
-//		System.out.println(Arrays.toString(aaa));
-////		
-////		8)
-//
-//		int[] intArray = { 1, 2,3,2, 3, 4, 5 ,2};
-//		int[] removed = ArrayUtils.removeElement(intArray, 2);// create a new
-//																// array
-//		System.out.println(Arrays.toString(removed));
-//	}
-//
-//}
+//		8)
+
+		int[] intArray = { 1, 2,3,2, 3, 4, 5 ,2};
+		int[] removed = ArrayUtils.removeElement(intArray, 2);// create a new
+																// array
+		System.out.println(Arrays.toString(removed));
+	}
+
+}
+
